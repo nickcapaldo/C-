6a840342ab28740f78b3e9b4100ff750bd752f75
nickcapaldo <68262105+nickcapaldo@users.noreply.github.com>
2020-07-14 14:24:47 +1000

    Add files via upload

---
+++ Check for leap year.cpp
@@ -0,0 +1,25 @@
+#incude <iostream>
+using namesoace std;
+
+int main()
+{
+    int year;
+     uYear << "Enter in a year: ";
+     cin >> year
+     if (year % 4 ==)
+     {
+         if (year % 100 == )
+         {
+             if (year % 400 == )
+               uYear << year << "The entered year is a leap year";  
+             else 
+             uYear << year << "The entered year is not a leap year";
+         }
+         else
+            uYear << year << "The entered year is a leap year"; 
+     }
+     else 
+        uYear << year << "The entered year is not a leap year";
+
+     return ;
+}
